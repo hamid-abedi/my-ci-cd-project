4991f3c2f7b19bbfa37ad988e3d16e3d763467b1
Hamid Abedi <hamidrabd@gmail.com>
2025-05-26 14:44:37 +0200

    Add csvtojson.py for converting CSV to JSON

--- csvtojson.py
+++ csvtojson.py
@@ -7,5 +7,6 @@ def csv_to_json(csv_file, json_file):
     with open(json_file, 'w', encoding='utf-8') as f:
         json.dump(data, f, indent=4)
 
+
 if __name__ == '__main__':
     csv_to_json('profiles1.csv', 'data.json')
